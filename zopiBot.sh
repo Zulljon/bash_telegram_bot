abe6c90b8a228aa74b0296f42cecce2493dd4296
zulljon <zulljondan@gmail.com>
2018-06-26 15:15:23 +0300

    fix according to https://www.shellcheck.net

--- zopiBot.sh
+++ zopiBot.sh
@@ -12,36 +12,36 @@ source opi_leds
 #sayru - say in speakers on russian
 
 if ! $DEBUG; then
-	echo "0" > ${SIG_to_DIE}
+	echo "0" > "$SIG_to_DIE"
 	[[ $(uname -n) == "orangepizero" ]] && init_leds
 	while true; do
 		# ask server for new massage to bot, if there is, give me 1 pls
 		get_Tlast_message
 		# update & save last_message_id, according to BOT API
-		if [[ $(UPDATE_ID) -eq $(cat ${LMidFILE} ) && $(UPDATE_ID) != "null" ]]; then
-			echo $(($(UPDATE_ID)+1)) > $LMidFILE
-
-			echo $(MESS_TEXT) > $CMD_ARGS
+		if [[ "$(UPDATE_ID)" -eq "$(cat "${LMidFILE}" )" && "$(UPDATE_ID)" != "null" ]]; then
+			# calc message_id for the next get_Tlast_message
+			echo "$(($(UPDATE_ID)+1))" > "$LMidFILE"
+			# find if text is command like message
 			parse_text
 
-			blink_led $LED4
+			blink_led "$LED4"
 			#wait $(jobs -p)
 		else
-			trig_led $LED5
+			trig_led "$LED5"
 			#wait $(jobs -p)
 		fi
 		#trig_led $LED1 &
 		#wait $(jobs -p)
 		#sleep 0.013
-		if [[ $(cat ${SIG_to_DIE}) == "9" ]]; then
+		if [[ "$(cat "${SIG_to_DIE}")" == "9" ]]; then
 			exit 0
 		fi
 	done
 else
-	LAST_MESSAGE_ID=$(cat $LMidFILE)
-	echo $LAST_MESSAGE_ID
+	LAST_MESSAGE_ID="$(cat "$LMidFILE")"
+	echo "$LAST_MESSAGE_ID"
 	echo "$(curl -s -X GET "${URL}/getUpdates?" -d offset="${LAST_MESSAGE_ID}" -d limit=1 )" > $RAM_JSON
-	echo $RAM_JSON
+	echo "$RAM_JSON"
 fi
 
 exit $?
